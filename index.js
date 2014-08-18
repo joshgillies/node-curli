89d178b858440de3139357f089ab1c68be132d54
Josh Gillies <github@joshgilli.es>
2014-08-18 19:03:38 +1000

    Default protocol should be http not https. Related to #4

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function buildUAString() {
 function curlI(opts, callback) {
   var options = typeof opts === 'string' ? url.parse(opts) : opts;
   if (!options.protocol) return callback(new Error('protocol not specified'));
-  var protocol = options.protocol === 'http:' ? require('http') : require('https');
+  var protocol = options.protocol === 'https:' ? require('https') : require('http');
   options.method = 'HEAD';
   options.headers = {
     'User-Agent': buildUAString()
